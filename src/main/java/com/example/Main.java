963ee167a446ec9720a90b829e8833da1bc1032f
SUilhl3 <ilagang@my.centennialcollege.ca>
2025-03-31 19:45:27 -0400

    - Suggested adding the multiplied sum of the users inputs. - Created examples with results within the project_details.txt - Added code to implement the results

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,16 +8,19 @@ public class Main {
         int a;
         int b;
         int c;
-        int sum;
-        System.out.println("This code is used to mutiply three numbers together.");
+        int addSum;
+        int multiSum;
+        System.out.println("This code is used to show the sum three numbers being added together as well as multiplied together.");
         System.out.println("Enter the first number: ");
         a = input.nextInt();
         System.out.println("Enter the second number");
         b = input.nextInt();
-        System.out.println("Enter the thrid number: ");
+        System.out.println("Enter the third number: ");
         c = input.nextInt();
-        sum = a*b;
-        System.out.println("Your sum is: " + sum);
+        addSum = a*b*c;
+        multiSum = 2+4;
+        System.out.println("Your added sum is: " + addSum);
+        System.out.println("Your multiplied sum is " + addSum);
 
     }
 }
