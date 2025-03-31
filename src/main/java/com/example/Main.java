6148a62635689adce6ced19d55afd36d1e1e22f0
SUilhl3 <ilagang@my.centennialcollege.ca>
2025-03-31 19:20:02 -0400

    forgetting to add words to files

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,23 @@
 package com.example;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int a;
+        int b;
+        int c;
+        int sum;
+        System.out.println("This code is used to mutiply three numbers together.");
+        System.out.println("Enter the first number: ");
+        a = input.nextInt();
+        System.out.println("Enter the second number");
+        b = input.nextInt();
+        System.out.println("Enter the thrid number: ");
+        c = input.nextInt();
+        sum = a*b;
+        System.out.println("Your sum is: " + sum);
+
     }
 }
